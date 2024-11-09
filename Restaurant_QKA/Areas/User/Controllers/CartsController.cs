3b6e0e91071aa35743b2d997a0eedf875a4f208c
Kiet1122 <166615603+Kiet1122@users.noreply.github.com>
2024-11-09 09:41:25 +0700

    K_9

---
+++ Restaurant_QKA/Areas/User/Controllers/CartsController.cs
@@ -0,0 +1,34 @@
+﻿using Restaurant_QKA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant_QKA.Areas.User.Controllers
+{
+    public class CartsController : Controller
+    {
+        RestaurantEntities db = new RestaurantEntities();
+        // GET: User/Carts
+        public ActionResult Index(int? cusId)
+        {
+            // Retrieve customer ID from the session or parameters if necessary
+            int customerId = cusId ?? 0; // Replace with actual customer ID retrieval method
+
+            // Fetch items in the cart for the specified customer
+            var cartItems = db.Carts
+                              .Where(c => c.CusID == customerId)
+                              .Select(c => new
+                              {
+                                  c.CartID,
+                                  c.ItemID,
+                                  c.Quantity,
+                                  c.CusID
+                              })
+                              .ToList();
+
+            return View(cartItems); // Pass data to the view
+        }
+    }
+}
